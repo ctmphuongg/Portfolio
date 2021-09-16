b9286b9677c21279e1f2ec0392e31d1f9e10b058
ctmphuongg <phuong.caothiminh.contact@gmail.com>
2021-09-16 17:59:26 +0700

    add sections

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/*show menu*/
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*hide menu*/
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*remove menu mobile*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+        // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
 const ideaTextTrans = {
     opacity: 0,
     y: -20,
@@ -77,3 +105,17 @@ tabs.forEach(tab => {
         tab.classList.add('achievements__active')
     })
 })
+
+var audio = document.getElementById('audio');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count = 0;
+
+function playPause() {
+    if (count == 0) {
+        count = 1
+        audio.play()
+    } else {
+        count = 0
+        audio.pause()
+    }
+}
